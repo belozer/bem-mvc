18fd8a0210fe1daf9408325e05ccfb1ce2f4cbfd
Sergey Belozyorcev <nextdrift@gmail.com>
2018-01-19 19:05:38 +0300

    up

--- common.blocks/glue-field/_type/glue-field_type_mod.js
+++ common.blocks/glue-field/_type/glue-field_type_mod.js
@@ -1,33 +1,34 @@
-modules.define('glue-field', ['i-bem__dom'], function(provide, BEMDOM) {
-
-provide(BEMDOM.decl({ block: 'glue-field', modName : 'type', modVal : 'mod'}, {
-
-    onSetMod: {
-        js: {
-            inited: function() {
-                this.__base();
-            }
-        }
-    },
-
-    init: function() {
-        this.__base.apply(this, arguments);
-        
-        this._gluedBlock = this[this.params.elem ? 'findBlockOutside' : 'findBlockOn'](this.domElem, this.params.block || this.__self.getName());
-        this._gluedElem = this.params.elem && this._gluedBlock.findElem(this.domElem, this.params.elem);
-
-        this._fieldType = this.model.getType(this.name);
-    },
-
-    onFieldChange: function(e, data) {
-        var modVal = this.model.get(this.name);
-
-        if (this._fieldType == 'boolean')
-            modVal = modVal ? true : false;
-
-        this._gluedBlock.setMod(this._gluedElem, this.params.modName, modVal);
-    }
-
-}));
-
-});
+// TODO
+// modules.define('glue-field', function(provide) {
+//
+// provide(GlueField.declMod({ modName : 'type', modVal : 'mod'}, {
+//
+//     onSetMod: {
+//         js: {
+//             inited: function() {
+//                 this.__base();
+//             }
+//         }
+//     },
+//
+//     init: function() {
+//         this.__base.apply(this, arguments);
+//
+//         this._gluedBlock = this[this.params.elem ? 'findParrentBlock' : 'findMixedBlock'](this.domElem, this.params.block || this.__self.getName());
+//         this._gluedElem = this.params.elem && this._gluedBlock.findElem(this.domElem, this.params.elem);
+//
+//         this._fieldType = this.model.getType(this.name);
+//     },
+//
+//     onFieldChange: function(e, data) {
+//         var modVal = this.model.get(this.name);
+//
+//         if (this._fieldType == 'boolean')
+//             modVal = modVal ? true : false;
+//
+//         this._gluedBlock.setMod(this._gluedElem, this.params.modName, modVal);
+//     }
+//
+// }));
+//
+// });
